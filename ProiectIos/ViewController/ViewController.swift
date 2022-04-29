8f4db65033389585c73918eb39983f48e7a698b6
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2022-04-29 14:44:34 +0200

    added more animations

--- ProiectIos/ViewController/ViewController.swift
+++ ProiectIos/ViewController/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBOutlet weak var tblFriends: UITableView!
     
+   
+    @IBOutlet weak var btnFriends: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +19,63 @@ class ViewController: UIViewController {
         title="Home"
         
     }
+    
+    @IBAction func touchDownFr(_ sender: UIButton) {
+        sender.pulsate()
+    }
+    
+    @IBAction func touchUpInsFr(_ sender: UIButton) {
+        sender.flash()
+    }
 }
-extension ViewController: UITableViewDelegate,UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5;
+extension UIButton {
+    
+    func pulsate() {
         
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 2
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: "pulse")
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=tableView.dequeueReusableCell(withIdentifier: "FriendCell",for:indexPath) as! FriendTableViewCell
-        return cell;
+  
+      func flash() {
+        
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.5
+        flash.fromValue = 1
+        flash.toValue = 0.1
+          flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 3
+        
+        layer.add(flash, forKey: nil)
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90;
+    
+  
+    func shake() {
+        
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        layer.add(shake, forKey: "position")
     }
 }
+
 

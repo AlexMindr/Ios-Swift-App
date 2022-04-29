161d4fa25d0573147bf3afd7b490c89487a9123c
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2022-04-29 16:32:56 +0200

    added gallery

---
+++ ProiectIos/ViewController/ImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageViewController.swift
+//  ProiectIos
+//
+//  Created by user217581 on 4/29/22.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     title="Share image"
+    }
+    
+
+   
+}
